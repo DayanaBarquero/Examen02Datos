e77898802c7ff0a85475c2504d974e67cb8faaf9
DayanaBarquero <68917196+DayanaBarquero@users.noreply.github.com>
2021-01-28 18:54:06 -0600

    cambios

--- Kruskal.h
+++ Kruskal.h
@@ -8,22 +8,11 @@
 #include "Graph.h"
 #include <vector>
 #include <algorithm>
-#include <unordered_map>
 using namespace std;
 class Kruskal {
 private:
     Graph *graph;
 public:
-    static string find(const string& vertex);
-    static void Union(const string& root1,const string& root2);
-    static void makeSet(const string& vertex);
-
-    Kruskal(Graph *graph);
-
-    Kruskal();
-
-    virtual ~Kruskal();
-    void kruskaMST();
 
 };
 
--- Kruskal2.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by ariad on 26/1/2021.
-//
-
-#include "Kruskal2.h"
